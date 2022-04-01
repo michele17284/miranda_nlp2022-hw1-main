d6a6aece56f61debf54d536e7992c210c18577b5
michele17284 <47VtY#2%Sj$YdFEv>
2022-04-01 21:01:08 +0200

    working model

--- hw1/stud/implementation.py
+++ hw1/stud/implementation.py
@@ -53,7 +53,7 @@ UNK_TOKEN = '<unk>'
 PAD_TOKEN = '<pad>'
 TRAIN_PATH = "./data/train.tsv"
 DEV_PATH = "./data/dev.tsv"
-MODEL_PATH = "./model/myModel.ckpt"
+MODEL_PATH = "./newModel/myModel.ckpt"
 
 
 #creating a vocabulary with glove embeddings
@@ -89,7 +89,11 @@ embeddings, word2idx = create_embeddings(glove)  # create and indexing embedding
 
 class SentenceDataset(Dataset):
 
-    def __init__(self,vectors,word2idx,sentences_path=None,sentences=None,lemmatization=True,test=False):
+    def __init__(self, vectors, word2idx, sentences_path=None, sentences=None, lemmatization=True,
+                 test=False, class2id={"O": 0, "B-PER": 1, "B-LOC": 2, "B-GRP": 3, "B-CORP": 4, "B-PROD": 5,
+                                       # indexing output classes
+                                       "B-CW": 6, "I-PER": 7, "I-LOC": 8, "I-GRP": 9, "I-CORP": 10, "I-PROD": 11,
+                                       "I-CW": 12, "<pad>": 13}):
         file_output = self.read_file(sentences_path) if sentences_path else self.read_sentences(sentences)
         self.embedding_vectors = vectors
         self.word2idx = word2idx
@@ -97,42 +101,40 @@ class SentenceDataset(Dataset):
         self.w_lemmatization = lemmatization
         self.word_count = dict()
         self.extract_sentences(file_output)
-        #self.remove_most_frequent(percentage=1)
+        # self.remove_most_frequent(percentage=1)
         # encoding of classes
-        self.class2id = {"O": 0, "B-PER": 1, "B-LOC": 2, "B-GRP": 3, "B-CORP": 4, "B-PROD": 5, #indexing output classes
-                    "B-CW": 6, "I-PER": 7, "I-LOC": 8, "I-GRP": 9, "I-CORP": 10, "I-PROD": 11, "I-CW": 12}
-
+        self.class2id = class2id
         self.id2class = {v: k for (k, v) in self.class2id.items()}
 
-    #function to remove the most frequent words in the corpus (ideally the less discriminative, only acting as noise, but
-    #it didn't work great so I'm not sure
-    def remove_most_frequent(self,percentage):
-        #print("STARTING MOST FREQUENT")
-        sorted_counts = sorted(self.word_count.items(),key=lambda x:x[1],reverse=True)  #sort dict with word counts
-        total_words = len(sorted_counts)                                                #check the total number of words
-        start_index = int((total_words/100)*percentage)                                 #compute the starting point to
-        sorted_counts = sorted_counts[start_index:]                                     #exclude the wanted percentage
-        sorted_counts = set([x[0] for x in sorted_counts])                              #transforming in set to make
-        sentences = []                                                                  #checking the presence efficient (O(1))
-        #print("STARTING REMOVING")
+    # function to remove the most frequent words in the corpus (ideally the less discriminative, only acting as noise, but
+    # it didn't work great so I'm not sure
+    def remove_most_frequent(self, percentage):
+        # print("STARTING MOST FREQUENT")
+        sorted_counts = sorted(self.word_count.items(), key=lambda x: x[1], reverse=True)  # sort dict with word counts
+        total_words = len(sorted_counts)  # check the total number of words
+        start_index = int((total_words / 100) * percentage)  # compute the starting point to
+        sorted_counts = sorted_counts[start_index:]  # exclude the wanted percentage
+        sorted_counts = set([x[0] for x in sorted_counts])  # transforming in set to make
+        sentences = []  # checking the presence efficient (O(1))
+        # print("STARTING REMOVING")
         for sample in self.sentences:
             sentence = sample[0]
             new_sentence = []
-            for word,pos in sentence:
+            for word, pos in sentence:
                 if word in sorted_counts:
-                    new_sentence.append((word,pos))
-            sentences.append((new_sentence,sample[1],sample[2]))
+                    new_sentence.append((word, pos))
+            sentences.append((new_sentence, sample[1], sample[2]))
         self.sentences = sentences
 
-
-    #little function to read a file given the path
-    def read_file(self,path):
+    # little function to read a file given the path
+    def read_file(self, path):
         sentences = list()
         with open(path) as file:
             tsv_file = csv.reader(file, delimiter="\t")
-            for idx,line in enumerate(tsv_file):
+            for idx, line in enumerate(tsv_file):
                 if len(line) > 0:
                     if line[0] == '#':
+
                         sentences.append(dict())
                         sentences[-1]["id"] = line[2]
                         sentences[-1]["text"] = []
@@ -140,12 +142,12 @@ class SentenceDataset(Dataset):
                     else:
                         sentences[-1]["text"].append(line[0])
                         sentences[-1]["labels"].append(line[1])
-        #print(sentences)
+        # print([len(sentences[i]['text']) for i in range(len(sentences))])
         return sentences
 
-    def read_sentences(self,sentences):
+    def read_sentences(self, sentences):
         sents = list()
-        for idx,line in enumerate(sentences):
+        for idx, line in enumerate(sentences):
             d = dict()
             d["id"] = idx
             d["text"] = line
@@ -153,33 +155,37 @@ class SentenceDataset(Dataset):
             sents.append(d)
         return sents
 
-    #function to extract the sentences from the dictionary of samples
-    def extract_sentences(self,file_output):
-        self.sentences = list()                 #creating a list to store the instances in the dataset
+    # function to extract the sentences from the dictionary of samples
+    def extract_sentences(self, file_output):
+        self.sentences = list()  # creating a list to store the instances in the dataset
         for instance in file_output:
-            processed = self.text_preprocess(instance)  #preprocessing of the sentence
-            labels = 'UNKNOWN'   #this is needed to make the system able to give a prediction without having a ground truth
-            if 'labels' in instance: #then if there is a ground truth we take it
+            processed = self.text_preprocess(instance)  # preprocessing of the sentence
+            labels = 'UNKNOWN'  # this is needed to make the system able to give a prediction without having a ground truth
+            if 'labels' in instance:  # then if there is a ground truth we take it
                 labels = processed['labels']
-            self.sentences.append((processed["text"], labels, id))           #append a triple (sentence,label,id) which are all the informations we need
-        if not self.test: random.Random(42).shuffle(self.sentences)         #for the training phase, shuffle data to avoid bias relative to data order
-        #print(self.sentences)
+            self.sentences.append((processed["text"], labels,
+                                   id))  # append a triple (sentence,label,id) which are all the informations we need
+        if not self.test: random.Random(42).shuffle(
+            self.sentences)  # for the training phase, shuffle data to avoid bias relative to data order
+        # print(self.sentences)
 
-    #function to convert the pos extracted by nltk to the pos required by the very same library for lemmatization
-    #I also use it to give pos='' to punctuation
-    def get_standard(self,pos):
+    # function to convert the pos extracted by nltk to the pos required by the very same library for lemmatization
+    # I also use it to give pos='' to punctuation
+    def get_standard(self, pos):
         if pos[0] == 'V': return wordnet.VERB
         if pos[0] == 'R': return wordnet.ADV
         if pos[0] == 'N': return wordnet.NOUN
         if pos[0] == 'J': return wordnet.ADJ
         return ''
 
-    #function which includes all the preprocessing steps for the sentences, which are tokenization,
-    #stopwords and punctuation removal,pos tagging and lemmatization
-    def text_preprocess(self,sentence):
+    # function which includes all the preprocessing steps for the sentences, which are tokenization,
+    # stopwords and punctuation removal,pos tagging and lemmatization
+    def text_preprocess(self, sentence):
         text = sentence["text"]
         labels = sentence["labels"]
-        sent = [(text[i],labels[i]) for i in range(len(text))]# if text[i] not in string.punctuation and text[i] not in stop_tokens]
+        sent = [(text[i], labels[i]) for i in
+                range(len(text))]  # if text[i] not in string.punctuation and text[i] not in stop_tokens]
+        # sent = [(text[i],labels[i]) for i in range(len(text)) if labels[i] != 'O']
         sentence["text"] = [pair[0] for pair in sent]
         sentence["labels"] = [pair[1] for pair in sent]
         return sentence
@@ -199,46 +205,45 @@ class SentenceDataset(Dataset):
         return [(word,pos) for word,pos in clean_standard2]
         '''
 
-
-
-
-    #function to return the number of instances contained in the dataset
+    # function to return the number of instances contained in the dataset
     def __len__(self):
         return len(self.sentences)
 
-    #function to get the i-th instance contained in the dataset
+    # function to get the i-th instance contained in the dataset
     def __getitem__(self, idx):
         return self.sentences[idx]
 
-    #custom dataloader which incorporates the collate function
-    def dataloader(self,batch_size):
-        return DataLoader(self,batch_size=batch_size,collate_fn=partial(self.collate,self.word2idx))
+    # custom dataloader which incorporates the collate function
+    def dataloader(self, batch_size):
+        return DataLoader(self, batch_size=batch_size, collate_fn=partial(self.collate, self.word2idx))
 
-        #function to map each lemma,pos in a sentence to their indexes
-    def sent2idx(self ,sent, word2idx):
-        #return torch.tensor([word2idx[word] for word,pos in sent]) #in the case i'm not using pos I just return a placeholder for pos
+        # function to map each lemma,pos in a sentence to their indexes
+
+    def sent2idx(self, sent, word2idx):
+        # return torch.tensor([word2idx[word] for word,pos in sent]) #in the case i'm not using pos I just return a placeholder for pos
         return torch.tensor([word2idx[word] for word in sent])
 
-
-
-    #custom collate function, used to create the batches to give as input to the nn
-    #it's needed because we are dealing with sentences of variable length and we need padding
-    #to be sure that each sentence in a batch has the same length, which is necessary since
-    #neural networks need fixed dimension inputs
-    def collate(self,word2idx, data):
-        X = [self.sent2idx(instance[0], word2idx) for instance in data]                             #extracting the input sentence
+    # custom collate function, used to create the batches to give as input to the nn
+    # it's needed because we are dealing with sentences of variable length and we need padding
+    # to be sure that each sentence in a batch has the same length, which is necessary since
+    # neural networks need fixed dimension inputs
+    def collate(self, word2idx, data):
+        X = [self.sent2idx(instance[0], word2idx) for instance in data]  # extracting the input sentence
         X_len = torch.tensor([x.size(0) for x in X], dtype=torch.long).to(device)
         y = [self.sent2idx(instance[1], self.class2id) for instance in data]
-        ids = [instance[2] for instance in data]                                                    #extracting the sentence ids
-        X = torch.nn.utils.rnn.pad_sequence(X, batch_first=True, padding_value=1).to(device)        #padding all the sentences to the maximum length in the batch (forcefully max_len)
-        y = torch.nn.utils.rnn.pad_sequence(y, batch_first=True, padding_value=13).to(device)              #extracting the ground truth
-        return X, X_len,y, ids
+        ids = [instance[2] for instance in data]  # extracting the sentence ids
+        X = torch.nn.utils.rnn.pad_sequence(X, batch_first=True, padding_value=1).to(
+            device)  # padding all the sentences to the maximum length in the batch (forcefully max_len)
+        y = torch.nn.utils.rnn.pad_sequence(y, batch_first=True, padding_value=self.class2id[PAD_TOKEN]).to(
+            device)  # extracting the ground truth
+        return X, X_len, y, ids
 
 def build_model(device: str) -> Model:
     # STUDENT: return StudentModel()
     # STUDENT: your model MUST be loaded on the device "device" indicates
 
-    my_model = StudentModel(embeddings,bidirectional=False,hidden1=128,hidden2=128,lstm_layers=5,p=0.5).to(device)         #instantiating the model
+    my_model = StudentModel(embeddings,bidirectional=False,hidden1=512,hidden2=128,lstm_layers=1,p=0.1,loss_fn=nn.CrossEntropyLoss(ignore_index=13),
+                     num_classes=14).to(device)
 
     my_model.load_state_dict(torch.load(MODEL_PATH, map_location=device))
     return my_model
@@ -273,67 +278,67 @@ class RandomBaseline(Model):
         ]
 
 
-class StudentModel(nn.Module,Model):
+# Model class
+# class StudentModel(nn.Module,Model):        #needed for testing
+class StudentModel(nn.Module):  # needed for training
 
-    # STUDENT: construct here your model
-    # this class should be loading your weights and vocabulary
     # constructor method, for which are needed the word embedding vectors, the dimensions of the two linear
     # layers, the dropout probabilty p, a flag to choose if the lstm layer must be bidirectonal, the number
     # of layers of the lstm layer and the loss function (but these last 4 already have a default value)
     def __init__(self, embeddings,  # word embedding vectors
                  pos_embeddings=None,  # pos embedding vectors
-                 hidden1=512,  # dimension of the first hidden layer
-                 hidden2=512,  # dimension of the second hidden layer
+                 hidden1=128,  # dimension of the first hidden layer
+                 hidden2=128,  # dimension of the second hidden layer
+                 # hidden3=128,
                  p=0.0,  # probability of dropour layer
                  bidirectional=False,  # flag to decide if the LSTM must be bidirectional
                  lstm_layers=1,  # layers of the LSTM
-                 loss_fn=torch.nn.CrossEntropyLoss(ignore_index=13)):  # loss function
+                 loss_fn=torch.nn.CrossEntropyLoss(ignore_index=0),
+                 num_classes=13):  # loss function
         super().__init__()
-        self.embedding = nn.Embedding.from_pretrained(embeddings)
+        self.embedding = nn.Embedding.from_pretrained(embeddings, freeze=False)
         self.pos_embeddings = None if pos_embeddings == None else nn.Embedding.from_pretrained(pos_embeddings,
                                                                                                freeze=False)  # choose if creating or not an embedding layer
-        hidden1 = hidden1 * 2 if bidirectional else hidden1  # based on wether pos embeddings were created
+        # based on wether pos embeddings were created
         input_dim = embeddings.size(1) if not self.pos_embeddings else embeddings.size(1) + pos_embeddings.size(
             1)  # or not
         self.lstm = nn.LSTM(input_size=input_dim, hidden_size=hidden1, num_layers=lstm_layers, batch_first=True,
                             bidirectional=bidirectional)
+        hidden1 = hidden1 * 2 if bidirectional else hidden1
         self.lin1 = nn.Linear(hidden1, hidden2)
-        self.lin2 = nn.Linear(hidden2, 13)
+        self.lin2 = nn.Linear(hidden2, num_classes)
+        # self.lin3 = nn.Linear(hidden3,num_classes)
         self.loss_fn = loss_fn
         self.dropout = nn.Dropout(p=p)
+        self.num_classes = num_classes
 
     # forward method, automatically called when calling the instance
     # it takes the Xs and their length in batches
     def forward(self, X, X_len):
         # lemmas,pos = X[...,0],X[...,1]                                  #separating pos from lemmas
-        embedding = self.embedding(X)  # expanding the words from indices to embedding vectors
+        embeddings = self.embedding(X)  # expanding the words from indices to embedding vectors
         '''
         if self.pos_embeddings != None:                                     #in the case I'm using pos embeddings, I pass their indexes through their own embedding layer
             pos_embeddings_out1 = self.pos_embeddings(pos)                 #and then concatenate them to the corresponding words
 
             embedding1 = torch.cat([embedding,pos_embeddings_out1],dim=-1)
         '''
-        lstm_out = self.lstm(embedding)[0]
-        batch_size, sentence_len, hidden_size = lstm_out.shape  # sentence length here is taken to remove padding
-        flattened1 = lstm_out.reshape(-1,
-                                      hidden_size)  # the output of the lstm is flattened (batch size,sentence length,lstm output dimension)->
-        #                                       (batch size,sentence length*lstm output dimension)
-        last_word_relative_indices = X_len - 1  # in order to have the last index of each sentence in the batch (excluding padding), we subtract one to the length
-        sentences_offsets = torch.arange(batch_size,
-                                         device=device) * sentence_len  # we compute the offsets, or absolute indexes, where each sentence starts
-        vec_sum_index = sentences_offsets + last_word_relative_indices  # we take the last token of each sentence, which is the hidden
-        # state of the LSTM which summarize the content of the sentence
-        # vec_sum = flattened1[vec_sum_index]                         #we use the index to retrieve the vector which summarizes the sentence
-        ##out = self.dropout(torch.cat([vec_sum1,vec_sum2],dim=-1))
-        out = self.dropout(flattened1)
+        # embeddings = embeddings.unsqueeze(1)
+        lstm_out = self.lstm(embeddings)[0]
+        lstm_out = lstm_out.squeeze()
+        out = self.dropout(lstm_out)
         out = torch.relu(out)
         # print(out.size())
         out = self.lin1(out)
-
-        # out = self.lin2(out)
+        out = self.dropout(out)
+        out = torch.relu(out)
+        out = self.lin2(out)
+        # out = self.dropout(out)
+        # out = torch.relu(out)
+        # out = self.lin3(out)
         out = out.squeeze(1)
         # print(out.size())
-        out = torch.softmax(out, dim=-1)
+        out = torch.softmax(out, dim=-1) if self.num_classes > 1 else torch.sigmoid(out)
         return out
 
 
